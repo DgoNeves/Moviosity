6b80906dd5f4080d6d789c44c22e93a0edc4a18f
Diogo Neves <kidix7@gmail.com>
2017-08-25 00:44:06 +0100

    DateFilter And Toggle

--- src/pages/movie-list-page/movie-list-page.ts
+++ src/pages/movie-list-page/movie-list-page.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController , NavParams } from 'ionic-angular';
 
+import * as _ from 'lodash';
+import * as moment from 'moment';
+
 @Component({
   templateUrl: 'movie-list-page.html'
 })
@@ -8,18 +11,44 @@ import { NavController , NavParams } from 'ionic-angular';
 
 export class MovieListPage {
 
+  dateFilter: string;
   movies = [
-    { Title: 'Test 1' , StartTime: '2017-08-23 23:10:00' , EndTime: '2017-08-23 23:10:00' },
-    { Title: 'Test 2' , StartTime: '2017-08-24 23:10:00' , EndTime: '2017-08-24 23:10:00' },
-    { Title: 'Test 3' , StartTime: '2017-08-25 23:10:00' , EndTime: '2017-08-25 23:10:00' },
+    { Title: 'Test 1' , StartTime: '2017-08-23' , EndTime: '2017-08-23 23:10:00' },
+    { Title: 'Test 2' , StartTime: '2017-08-24' , EndTime: '2017-08-24 23:10:00' },
+    { Title: 'Test 3' , StartTime: '2017-08-26' , EndTime: '2017-08-25 23:10:00' },
+    { Title: 'Test 4' , StartTime: '2017-09-26' , EndTime: '2017-08-25 23:10:00' },
   ];
 
+  allMovies: any[];
   channel: any;
-  constructor(public navCtrl: NavController , private navParams: NavParams) {
-    
-    this.channel = this.navParams.data;
+  useDateFilter = false;
 
-  } 
+  constructor(
+    private navCtrl: NavController , 
+    private navParams: NavParams) {  } 
+
+
+    ionViewCanEnter(){
+      this.channel = this.navParams.data;
+      this.allMovies = this.movies;
+    }
+
+  ionViewDidLoad(){
+    
+  }
+
+  dateChanged(){
+
+    if(this.useDateFilter)
+    {
+      this.movies = _.filter(this.allMovies, m => moment(m.StartTime , "YYYY-MM-DD").isSame(this.dateFilter, 'day'));
+    }
+    else
+    {
+      this.movies = this.allMovies;
+    }
+   
+  }
 
   goHome()
   {
